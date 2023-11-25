8f58886ac622cc69a2c0719d1ba63e33070e3876
Luiz Alencar <lf.dacol@gmail.com>
2023-11-24 23:29:38 -0300

    feat: implementa botao para atualizar cotacao

--- src/pages/Carteira.tsx
+++ src/pages/Carteira.tsx
@@ -4,23 +4,42 @@ import ConsolidadoRendaVariavel from "../components/Carteira/ConsolidadoRendaVar
 import { CarteiraService } from "../services/CarteiraService";
 import { CarteiraRendaVariavel } from "../interfaces/CarteiraRendaVariavel";
 import { CarteiraRendaFixa } from "../interfaces/CarteiraRendaFixa";
+import { faRotate } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Carteira() {
+  const [priceUpdated, setPriceUpdated] = useState<boolean>(true);
   const [carteira, setCarteira] = useState<
     (CarteiraRendaFixa | CarteiraRendaVariavel)[]
   >([]);
 
   useEffect(() => {
+    if (!priceUpdated) return;
+
     const fetchData = async () => {
       const data = await CarteiraService.getConsolidado();
       setCarteira(data);
     };
 
     fetchData();
-  }, []);
+    setPriceUpdated(false);
+  }, [priceUpdated]);
+
+  const atualizaCotacao = async () => {
+    await CarteiraService.updatePrices();
+    setPriceUpdated(true);
+  };
 
   return (
     <>
+      <div className="flex flex-row-reverse pr-6 pt-5">
+        <button
+          className="bg-green-600 text-white px-3 py-2 rounded-lg shadow-lg text-lg flex items-center"
+          onClick={() => atualizaCotacao()}
+        >
+          <FontAwesomeIcon icon={faRotate} />
+        </button>
+      </div>
       <main className="h-full">
         {/* Main Content */}
         <ConsolidadoRendaVariavel
--- src/services/CarteiraService.ts
+++ src/services/CarteiraService.ts
@@ -2,13 +2,38 @@ import { CarteiraRendaFixa } from "../interfaces/CarteiraRendaFixa";
 import { CarteiraRendaVariavel } from "../interfaces/CarteiraRendaVariavel";
 import { AxiosClient } from "../providers/AxiosClient";
 
-const getConsolidado = async () => {
+const getConsolidado = async (): Promise<
+  (CarteiraRendaVariavel | CarteiraRendaFixa)[]
+> => {
   let carteira = await AxiosClient.get<
     (CarteiraRendaVariavel | CarteiraRendaFixa)[]
   >("v1/carteira");
-  return carteira.data;
+
+  return carteira.data.map((c) => {
+    return {
+      ...c,
+      dataHoraCotacao: new Date(c.dataHoraCotacao),
+    };
+  });
+};
+
+const updatePrices = async (): Promise<boolean> => {
+  const cotacaoRendaFixa = AxiosClient.patch<boolean>(
+    "v1/renda-fixa/ativos/update-prices"
+  );
+  const cotacaoRendaVariavel = AxiosClient.patch<boolean>(
+    "v1/renda-variavel/ativos/update-prices"
+  );
+
+  const cotacaoAtualizada = await Promise.all([
+    cotacaoRendaFixa,
+    cotacaoRendaVariavel,
+  ]);
+
+  return cotacaoAtualizada.some((c) => !!c);
 };
 
 export const CarteiraService = {
   getConsolidado,
+  updatePrices,
 };
