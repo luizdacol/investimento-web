052ce93a7c3888af1f4ed19e595bd54a24bf63bb
Luiz Alencar <lf.dacol@gmail.com>
2023-12-23 11:34:52 -0300

    feat: configura axios para não lancar excecao para nenhum status

--- src/providers/AxiosClient.ts
+++ src/providers/AxiosClient.ts
@@ -1,3 +1,6 @@
 import axios from "axios";
 
-export const AxiosClient = axios.create({ baseURL: "http://localhost:3001" });
+export const AxiosClient = axios.create({
+  baseURL: "http://localhost:3001",
+  validateStatus: (status) => true,
+});
