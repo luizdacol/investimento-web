988eeed38d65b3df172b382adbaaca370af17bb7
Luiz Alencar <lf.dacol@gmail.com>
2024-10-19 15:58:36 -0300

    feat: implementa multiselect

--- src/components/Graficos/YieldOnCostLineChart.tsx
+++ src/components/Graficos/YieldOnCostLineChart.tsx
@@ -10,7 +10,9 @@ import {
   Legend,
   Line,
   Brush,
+  ResponsiveContainer,
 } from "recharts";
+import { MultiSelect } from "primereact/multiselect";
 
 const YieldOnCostLineChart = () => {
   const [yieldOnCostMensal, setYieldOnCostMensal] = useState<
@@ -20,6 +22,8 @@ const YieldOnCostLineChart = () => {
     start?: number;
     end?: number;
   }>({ start: 0, end: 10 });
+
+  const [selectedTickers, setSelectedTickers] = useState<string[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,36 +38,59 @@ const YieldOnCostLineChart = () => {
   return (
     <>
       <div className="flex space-x-2">
-        <div className="border w-4/5 h-[34rem] border-gray-200 bg-white py-2 px-4 rounded-md flex">
-          <LineChart width={1300} height={600} data={yieldOnCostMensal}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              dataKey="data"
-              type="category"
-              allowDuplicatedCategory={false}
-            />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            {yieldOnCostMensal.length &&
-              Object.keys(yieldOnCostMensal[0])
-                .filter((k) => k !== "data")
-                .map((yoc) => (
-                  <Line type="monotone" dataKey={yoc} name={yoc} key={yoc} />
-                ))}
-            <Brush
-              dataKey="data"
-              height={30}
-              stroke="#5dade2"
-              startIndex={brushIndex.start}
-              endIndex={brushIndex.end}
-              onDragEnd={(p) => {
-                if ("startIndex" in p) {
-                  setBrushIndex({ start: p.startIndex, end: p.endIndex });
-                }
-              }}
-            />
-          </LineChart>
+        <div className="border border-gray-200 bg-white w-1/6 h-[34rem] p-4 overflow-y-auto">
+          <MultiSelect
+            value={selectedTickers}
+            onChange={(e) => setSelectedTickers(e.value)}
+            filter
+            resetFilterOnHide
+            options={
+              yieldOnCostMensal.length
+                ? Object.keys(yieldOnCostMensal[0])
+                    .filter((k) => k !== "data")
+                    .map((yoc) => {
+                      return { label: yoc, value: yoc };
+                    })
+                : []
+            }
+            display="chip"
+            placeholder="Ativos"
+            maxSelectedLabels={3}
+            className="w-full md:w-20rem"
+          />
+        </div>
+        <div className="border w-5/6 h-[34rem] border-gray-200 bg-white py-2 px-4 rounded-md flex">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart width={1300} height={600} data={yieldOnCostMensal}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="data"
+                type="category"
+                allowDuplicatedCategory={false}
+              />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              {yieldOnCostMensal.length &&
+                Object.keys(yieldOnCostMensal[0])
+                  .filter((k) => selectedTickers.includes(k))
+                  .map((yoc) => (
+                    <Line type="monotone" dataKey={yoc} name={yoc} key={yoc} />
+                  ))}
+              <Brush
+                dataKey="data"
+                height={30}
+                stroke="#5dade2"
+                startIndex={brushIndex.start}
+                endIndex={brushIndex.end}
+                onDragEnd={(p) => {
+                  if ("startIndex" in p) {
+                    setBrushIndex({ start: p.startIndex, end: p.endIndex });
+                  }
+                }}
+              />
+            </LineChart>
+          </ResponsiveContainer>
         </div>
       </div>
     </>
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/lara-light-blue/theme.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter> 
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <PrimeReactProvider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </PrimeReactProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
