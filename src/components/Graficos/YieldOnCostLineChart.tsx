f20a2a922ba1c9a5227cdaf2b48ec77803883c16
Luiz Alencar <lf.dacol@gmail.com>
2024-10-19 17:26:03 -0300

    feat: customizacao do grafico de yield

--- src/components/Graficos/YieldOnCostLineChart.tsx
+++ src/components/Graficos/YieldOnCostLineChart.tsx
@@ -35,63 +35,111 @@ const YieldOnCostLineChart = () => {
     fetchData();
   }, []);
 
+  const COLORS = [
+    "#0088FE",
+    "#FF8042",
+    "#E74C3C",
+    "#00C49F",
+    "#FFBB28",
+    "#A569BD",
+    "#C8E309",
+    "#979A9A",
+    "#7B241C",
+    "#154360",
+    "#145A32",
+    "#7E5109",
+    "#490066",
+  ];
+
   return (
     <>
-      <div className="flex space-x-2">
-        <div className="border border-gray-200 bg-white w-1/6 h-[34rem] p-4 overflow-y-auto">
-          <MultiSelect
-            value={selectedTickers}
-            onChange={(e) => setSelectedTickers(e.value)}
-            filter
-            resetFilterOnHide
-            options={
-              yieldOnCostMensal.length
-                ? Object.keys(yieldOnCostMensal[0])
-                    .filter((k) => k !== "data")
-                    .map((yoc) => {
-                      return { label: yoc, value: yoc };
-                    })
-                : []
-            }
-            display="chip"
-            placeholder="Ativos"
-            maxSelectedLabels={3}
-            className="w-full md:w-20rem"
-          />
-        </div>
-        <div className="border w-5/6 h-[34rem] border-gray-200 bg-white py-2 px-4 rounded-md flex">
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart width={1300} height={600} data={yieldOnCostMensal}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis
-                dataKey="data"
-                type="category"
-                allowDuplicatedCategory={false}
-              />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              {yieldOnCostMensal.length &&
-                Object.keys(yieldOnCostMensal[0])
-                  .filter((k) => selectedTickers.includes(k))
-                  .map((yoc) => (
-                    <Line type="monotone" dataKey={yoc} name={yoc} key={yoc} />
-                  ))}
-              <Brush
-                dataKey="data"
-                height={30}
-                stroke="#5dade2"
-                startIndex={brushIndex.start}
-                endIndex={brushIndex.end}
-                onDragEnd={(p) => {
-                  if ("startIndex" in p) {
-                    setBrushIndex({ start: p.startIndex, end: p.endIndex });
-                  }
-                }}
-              />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
+      <div className="border h-[40rem] border-gray-200 bg-white py-2 px-4 rounded-md space-y-2">
+        <MultiSelect
+          value={selectedTickers}
+          onChange={(e) => setSelectedTickers(e.value)}
+          filter
+          resetFilterOnHide
+          showClear
+          showSelectAll={false}
+          options={
+            yieldOnCostMensal.length
+              ? Object.keys(yieldOnCostMensal[0])
+                  .filter((k) => k !== "data")
+                  .sort()
+                  .map((yoc) => {
+                    return { label: yoc, value: yoc };
+                  })
+              : []
+          }
+          display="chip"
+          placeholder="Ativos"
+          maxSelectedLabels={7}
+          itemClassName="text-xs"
+          className="w-1/2 text-xs ml-16"
+        />
+        <ResponsiveContainer width="100%" height="90%">
+          <LineChart
+            width={1300}
+            height={600}
+            data={yieldOnCostMensal}
+            margin={{
+              top: 20,
+              right: 20,
+              left: 5,
+              bottom: 0,
+            }}
+          >
+            <text
+              x="50%"
+              y="1%"
+              fill="black"
+              textAnchor="middle"
+              dominantBaseline="central"
+            >
+              <tspan
+                fontSize="14"
+                fontFamily="sans-serif"
+                fontWeight="bold"
+                fill="#727575"
+              >
+                Yield on Cost
+              </tspan>
+            </text>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="data"
+              type="category"
+              allowDuplicatedCategory={false}
+            />
+            <YAxis />
+            <Tooltip formatter={(value, name, props) => `${value}%`} />
+            <Legend />
+            {yieldOnCostMensal.length &&
+              Object.keys(yieldOnCostMensal[0])
+                .filter((k) => selectedTickers.includes(k))
+                .map((yoc, index) => (
+                  <Line
+                    type="monotone"
+                    dataKey={yoc}
+                    name={yoc}
+                    key={yoc}
+                    stroke={COLORS[index]}
+                  />
+                ))}
+            <Brush
+              dataKey="data"
+              height={30}
+              stroke="#5dade2"
+              startIndex={brushIndex.start}
+              endIndex={brushIndex.end}
+              onDragEnd={(p) => {
+                if ("startIndex" in p) {
+                  setBrushIndex({ start: p.startIndex, end: p.endIndex });
+                }
+              }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     </>
   );
