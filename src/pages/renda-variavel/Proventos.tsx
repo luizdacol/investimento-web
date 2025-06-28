0fab04acbfd10cef5adcce1e6eb51a729f5da2e7
Luiz Alencar <lf.dacol@gmail.com>
2025-06-27 22:14:27 -0300

    refactor: implementa nova tabela na pagina de proventos

--- src/pages/renda-variavel/Proventos.tsx
+++ src/pages/renda-variavel/Proventos.tsx
@@ -1,41 +1,67 @@
 import { useEffect, useState } from "react";
-import Table from "../../components/Table/Table";
 import { RendaVariavelService } from "../../services/RendaVariavelService";
 import { ProventoRendaVariavel } from "../../interfaces/Provento";
-import DateCell from "../../components/Table/DateCell";
-import PriceCell from "../../components/Table/PriceCell";
-import Cell from "../../components/Table/Cell";
-import ActionCell from "../../components/Table/ActionCell";
 import { useNavigate } from "react-router-dom";
-import { useStyles } from "../../hooks/useStyles";
-import { useSort } from "../../hooks/useSort";
 import { usePaginator } from "../../hooks/usePaginator";
 import { PaginatedDto } from "../../interfaces/PaginatedDto";
 import { Paginator } from "primereact/paginator";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { useTable } from "../../hooks/useTable";
 
 function Proventos() {
-  const { rowDefaultStyle } = useStyles();
-  const { sort } = useSort();
   const { take, skip, initialPaginatedObject, onPageChange } = usePaginator();
+  const { formatDateCell, formatPriceCell, formatActionCell, formatHeader } =
+    useTable();
   const [proventos, setProventos] = useState<
     PaginatedDto<ProventoRendaVariavel>
   >(initialPaginatedObject);
   const [reload, setReload] = useState<Boolean>(false);
   const navigate = useNavigate();
 
-  const headers = [
-    { key: "dataCom", label: "Data Com" },
-    { key: "dataPagamento", label: "Data Pagamento" },
-    { key: "ticker", label: "Ticker" },
-    { key: "valorBruto", label: "Valor Bruto" },
-    { key: "valorLiquido", label: "Valor Liquido" },
-    { key: "posicao", label: "Posição" },
-    { key: "valorTotal", label: "Valor total" },
-    { key: "tipo", label: "Tipo" },
-    { key: undefined, label: "Ações" },
+  const columns = [
+    {
+      field: "dataCom",
+      title: "Data Com",
+      content: (prov: ProventoRendaVariavel) => formatDateCell(prov.dataCom),
+    },
+    {
+      field: "dataPagamento",
+      title: "Data Pagamento",
+      content: (prov: ProventoRendaVariavel) =>
+        formatDateCell(prov.dataPagamento),
+    },
+    { field: "ticker", title: "Ticker" },
+    {
+      field: "valorBruto",
+      title: "Valor Bruto",
+      content: (prov: ProventoRendaVariavel) =>
+        formatPriceCell(prov.valorBruto),
+    },
+    {
+      field: "valorLiquido",
+      title: "Valor Liquido",
+      content: (prov: ProventoRendaVariavel) =>
+        formatPriceCell(prov.valorLiquido),
+    },
+    { field: "posicao", title: "Posição" },
+    {
+      field: "valorTotal",
+      title: "Valor total",
+      content: (prov: ProventoRendaVariavel) =>
+        formatPriceCell(prov.valorTotal),
+    },
+    { field: "tipo", title: "Tipo" },
+    {
+      field: undefined,
+      title: "Ações",
+      content: (prov: ProventoRendaVariavel) =>
+        formatActionCell(prov.id, handleDelete, handleUpdate),
+    },
   ];
 
   useEffect(() => {
+    console.log(take);
     const fetchData = async () => {
       const data = await RendaVariavelService.getProventos(take, skip);
       setProventos(data);
@@ -56,64 +82,47 @@ function Proventos() {
     navigate(`/renda-variavel/form-proventos?id=${id}`);
   };
 
-  const handleSort = (property: string, order: string) => {
-    const keyProperty = property as keyof ProventoRendaVariavel;
-    const sortedOperation = sort(proventos.content, keyProperty, order);
-
-    setProventos({ content: sortedOperation, metadata: proventos?.metadata });
-  };
-
   return (
     <>
       <main className="h-full">
         {/* Main Content */}
         <div className="mainCard">
           <div className="border w-full border-gray-200 bg-white py-4 px-6 rounded-md">
-            <Table
-              headers={headers}
-              title="Proventos"
-              newItemRedirect="/renda-variavel/form-proventos"
-              handleSort={handleSort}
+            <DataTable
+              value={proventos.content}
+              header={formatHeader(
+                "Proventos",
+                "/renda-variavel/form-proventos"
+              )}
+              size="small"
+              stripedRows
+              sortMode="multiple"
+              removableSort
+              paginatorTemplate={{
+                layout:
+                  "FirstPageLink PageLinks LastPageLink RowsPerPageDropdown",
+              }}
             >
-              {proventos.content.map((provento, index) => (
-                <tr key={index} className={rowDefaultStyle}>
-                  <DateCell cellValue={provento.dataCom} dataLabel="DataCom" />
-                  <DateCell
-                    cellValue={provento.dataPagamento}
-                    dataLabel="DataPagamento"
-                  />
-                  <Cell cellValue={provento.ticker} dataLabel="Ticker" />
-                  <PriceCell
-                    cellValue={provento.valorBruto}
-                    dataLabel="ValorBruto"
-                  />
-                  <PriceCell
-                    cellValue={provento.valorLiquido}
-                    dataLabel="ValorLiquido"
-                  />
-                  <Cell
-                    cellValue={provento.posicao.toString()}
-                    dataLabel="Posicao"
-                  />
-                  <PriceCell
-                    cellValue={provento.valorTotal}
-                    dataLabel="ValorTotal"
-                  />
-                  <Cell cellValue={provento.tipo} dataLabel="Tipo" />
-                  <ActionCell
-                    id={provento.id}
-                    handleDelete={handleDelete}
-                    handleUpdate={handleUpdate}
-                  ></ActionCell>
-                </tr>
+              {columns.map((column, index) => (
+                <Column
+                  key={column.field}
+                  field={column.field}
+                  header={column.title}
+                  body={column.content}
+                  sortable
+                  alignHeader="center"
+                  headerClassName="text-sm"
+                  align="center"
+                  bodyClassName="text-xs"
+                />
               ))}
-            </Table>
+            </DataTable>
             {proventos.content && (
               <Paginator
                 first={skip}
                 rows={take}
                 totalRecords={proventos.metadata.totalRecords}
-                rowsPerPageOptions={[50, 100, 200]}
+                rowsPerPageOptions={[50, 100, 500, 1000]}
                 onPageChange={onPageChange}
                 template={{
                   layout:
