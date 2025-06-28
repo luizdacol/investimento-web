bdbc19f744f010693a132e8f841e3004bc115d64
Luiz Alencar <lf.dacol@gmail.com>
2025-06-27 21:58:53 -0300

    refactor: implementa nova tabela na pagina de operacoes

---
+++ src/hooks/useTable.tsx
@@ -0,0 +1,90 @@
+import { faPencil, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button } from "primereact/button";
+import { Link } from "react-router-dom";
+
+export const useTable = () => {
+  const formatDateCell = (data: Date) => {
+    const isDataFutura = data > new Date();
+    const negrito = isDataFutura ? "font-bold" : "font-medium";
+    return (
+      <span className={negrito}>
+        {data.toLocaleDateString("pt-BR", { timeZone: "UTC" })}
+      </span>
+    );
+  };
+
+  const formatPriceCell = (price: number) => {
+    const defineCurrencyOptions = (
+      maximumFractionDigits: number = 2
+    ): Intl.NumberFormatOptions => {
+      return {
+        style: "currency",
+        currency: "BRL",
+        minimumFractionDigits: 2,
+        maximumFractionDigits,
+      };
+    };
+
+    return (
+      <span title={price.toLocaleString("pt-BR", defineCurrencyOptions(8))}>
+        {price.toLocaleString("pt-BR", defineCurrencyOptions())}
+      </span>
+    );
+  };
+
+  const formatActionCell = (
+    itemId: number,
+    handleDelete: (id: number) => Promise<void>,
+    handleUpdate: (id: number) => Promise<void>
+  ) => {
+    return (
+      <>
+        <span
+          className="text-sky-700 inline-flex py-1 px-2 rounded text-sm cursor-pointer"
+          onClick={(e) => {
+            handleUpdate(itemId);
+          }}
+        >
+          <FontAwesomeIcon icon={faPencil} />
+        </span>
+        <span
+          className="text-red-700 inline-flex py-1 px-2 rounded text-sm cursor-pointer"
+          onClick={(e) => {
+            handleDelete(itemId);
+          }}
+        >
+          <FontAwesomeIcon icon={faTrash} />
+        </span>
+      </>
+    );
+  };
+
+  const formatHeader = (title: string, linkToAddItem: string = "") => {
+    return (
+      <div className="flex flex-wrap gap-2 ">
+        <span className="text-xl text-900 font-bold text-center flex-1">
+          {title}
+        </span>
+        {linkToAddItem && (
+          <Link to={linkToAddItem}>
+            <Button
+              icon={<FontAwesomeIcon icon={faPlus} />}
+              rounded
+              raised
+              size="large"
+              className="bg-emerald-500 border-emerald-500"
+            />
+          </Link>
+        )}
+      </div>
+    );
+  };
+
+  return {
+    formatHeader,
+    formatDateCell,
+    formatPriceCell,
+    formatActionCell,
+  };
+};
--- src/pages/renda-variavel/Operacoes.tsx
+++ src/pages/renda-variavel/Operacoes.tsx
@@ -1,22 +1,18 @@
 import { useEffect, useState } from "react";
 import { RendaVariavelService } from "../../services/RendaVariavelService";
 import { OperacaoRendaVariavel } from "../../interfaces/Operacao";
-import ActionCell from "../../components/Table/ActionCell";
-import Cell from "../../components/Table/Cell";
-import DateCell from "../../components/Table/DateCell";
-import PriceCell from "../../components/Table/PriceCell";
-import Table from "../../components/Table/Table";
 import { useNavigate } from "react-router-dom";
-import { useStyles } from "../../hooks/useStyles";
-import { useSort } from "../../hooks/useSort";
 import { Paginator } from "primereact/paginator";
 import { PaginatedDto } from "../../interfaces/PaginatedDto";
 import { usePaginator } from "../../hooks/usePaginator";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { useTable } from "../../hooks/useTable";
 
 function Operacoes() {
-  const { rowDefaultStyle } = useStyles();
-  const { sort } = useSort();
   const { take, skip, initialPaginatedObject, onPageChange } = usePaginator();
+  const { formatDateCell, formatPriceCell, formatActionCell, formatHeader } =
+    useTable();
 
   const [operacoes, setOperacoes] = useState<
     PaginatedDto<OperacaoRendaVariavel>
@@ -24,15 +20,32 @@ function Operacoes() {
   const [reload, setReload] = useState<Boolean>(false);
   const navigate = useNavigate();
 
-  const headers = [
-    { key: "data", label: "Data" },
-    { key: "ticker", label: "Ticker" },
-    { key: "precoUnitario", label: "Preço Unitario" },
-    { key: "quantidade", label: "Quantidade" },
-    { key: "precoTotal", label: "Preço Total" },
-    { key: "tipoOperacao", label: "Tipo de Operação" },
-    { key: "tipoAtivo", label: "Tipo de Ativo" },
-    { key: undefined, label: "Ações" },
+  const columns = [
+    {
+      field: "data",
+      title: "Data",
+      content: (op: OperacaoRendaVariavel) => formatDateCell(op.data),
+    },
+    { field: "ticker", title: "Ticker" },
+    {
+      field: "precoUnitario",
+      title: "Preço Unitario",
+      content: (op: OperacaoRendaVariavel) => formatPriceCell(op.precoUnitario),
+    },
+    { field: "quantidade", title: "Quantidade" },
+    {
+      field: "precoTotal",
+      title: "Preço Total",
+      content: (op: OperacaoRendaVariavel) => formatPriceCell(op.precoTotal),
+    },
+    { field: "tipoOperacao", title: "Tipo de Operação" },
+    { field: "tipoAtivo", title: "Tipo de Ativo" },
+    {
+      field: undefined,
+      title: "Ações",
+      content: (op: OperacaoRendaVariavel) =>
+        formatActionCell(op.id, handleDelete, handleUpdate),
+    },
   ];
 
   useEffect(() => {
@@ -57,60 +70,47 @@ function Operacoes() {
     navigate(`/renda-variavel/form-operacoes?id=${id}`);
   };
 
-  const handleSort = (property: string, order: string) => {
-    const keyProperty = property as keyof OperacaoRendaVariavel;
-    const sortedOperation = sort(operacoes.content, keyProperty, order);
-
-    setOperacoes({ content: sortedOperation, metadata: operacoes?.metadata });
-  };
-
   return (
     <>
       <main className="h-full">
         {/* Main Content */}
         <div className="mainCard">
           <div className="border w-full border-gray-200 bg-white py-4 px-6 rounded-md">
-            <Table
-              headers={headers}
-              title="Operações"
-              newItemRedirect="/renda-variavel/form-operacoes"
-              handleSort={handleSort}
+            <DataTable
+              value={operacoes.content}
+              header={formatHeader(
+                "Operações",
+                "/renda-variavel/form-operacoes"
+              )}
+              size="small"
+              stripedRows
+              sortMode="multiple"
+              removableSort
+              paginatorTemplate={{
+                layout:
+                  "FirstPageLink PageLinks LastPageLink RowsPerPageDropdown",
+              }}
             >
-              {operacoes.content.map((operacao, index) => (
-                <tr key={index} className={rowDefaultStyle}>
-                  <DateCell cellValue={operacao.data} dataLabel="Data" />
-                  <Cell cellValue={operacao.ticker} dataLabel="Ticker" />
-                  <PriceCell
-                    cellValue={operacao.precoUnitario}
-                    dataLabel="PrecoUnitario"
-                  />
-                  <Cell
-                    cellValue={operacao.quantidade.toString()}
-                    dataLabel="Quantidade"
-                  />
-                  <PriceCell
-                    cellValue={operacao.precoTotal}
-                    dataLabel="PrecoTotal"
-                  />
-                  <Cell
-                    cellValue={operacao.tipoOperacao}
-                    dataLabel="Operacao"
-                  />
-                  <Cell cellValue={operacao.tipoAtivo} dataLabel="Tipo" />
-                  <ActionCell
-                    id={operacao.id}
-                    handleDelete={handleDelete}
-                    handleUpdate={handleUpdate}
-                  ></ActionCell>
-                </tr>
+              {columns.map((column, index) => (
+                <Column
+                  key={column.field}
+                  field={column.field}
+                  header={column.title}
+                  body={column.content}
+                  sortable
+                  alignHeader="center"
+                  headerClassName="text-sm"
+                  align="center"
+                  bodyClassName="text-xs"
+                />
               ))}
-            </Table>
+            </DataTable>
             {operacoes.content && (
               <Paginator
                 first={skip}
                 rows={take}
                 totalRecords={operacoes.metadata.totalRecords}
-                rowsPerPageOptions={[50, 100, 200]}
+                rowsPerPageOptions={[50, 100, 500, 1000]}
                 onPageChange={onPageChange}
                 template={{
                   layout:
