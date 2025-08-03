69e07d9beff4e4c494aebce87ccbcf12f23ffbec
Luiz Alencar <lf.dacol@gmail.com>
2025-08-03 19:19:42 -0300

    feat: organiza tipos e classes em enum

--- src/data/enums.ts
+++ src/data/enums.ts
@@ -12,3 +12,29 @@ export enum TipoProvento {
   RESTITUICAO_CAPITAL = "Restituição de Capital",
   RENDIMENTO_TRIBUTADO = "Rendimento Tributado",
 }
+
+export enum TipoAtivoRV {
+  ACAO = "Ação",
+  FII = "FII",
+  FIAGRO = "FiAgro",
+  BDR = "BDR",
+  ETF = "ETF",
+}
+
+export enum TipoAtivoRF {
+  TESOURO_DIRETO = "TesouroDireto",
+  CDB = "CDB",
+}
+
+export enum TipoAtivoCripto {
+  BTC = "Bitcoin",
+}
+
+export enum ClasseAtivo {
+  BOLSA_BRASILEIRA = "Bolsa Brasileira",
+  BOLSA_AMERICANA = "Bolsa Americana",
+  FUNDO_IMOBILIARIO = "Fundo Imobiliário",
+  TESOURO_DIRETO = "Tesouro Direto",
+  CDB = "CDB",
+  CRIPTOMOEDA = "Criptomoeda",
+}
